fc746e8420ed8f749a31d7d3a5309c20fcbe858a
AryanSingh <aryan@iitgn.ac.in>
2015-09-02 15:28:07 +0530

    testing product

--- assets/scripts/four_line.js
+++ assets/scripts/four_line.js
@@ -1,7 +1,21 @@
-$(document).ready(function(){
- $('div').click(function(){
-      $('div').animate({top:'600px'},300);
- $("div").append("<div id='#selector'></div>");
+(function(){
+  var Game;
+  Game = {
+    init:function(){
+      return $('#four_line .balls').click(function(){
+        var cellToAdd, colNum, currentColor;
+        colNum = this.dataset.colNum;
+        currentColor = Game.colors[Game.currentPlayer];
+        cellToAdd=$('.balls[data-col-num="' + colNum + '"][data-cell-type="none"]').last();
+        cellToAdd[0].dataset.cellType = currentColor;
+        cellToAdd.find('.slot').addClass(currentColor).addClassf('bounceInDown');
+        return Game.currentPlayer = (Game.currentPlayer +1)%2;
+      });
+    },
+    currentPlayer:0,
+    colors:['green','red']
+  };
+  $(function() {
+    return Game.init();
   });
- 
-});    
+}).call(this);
