afedfa096a7c948e072138cc9cd7d701558be586
AryanSingh <aryan@iitgn.ac.in>
2015-09-05 00:45:41 +0530

    horizontal and vertical check both working

--- assets/scripts/four_line.js
+++ assets/scripts/four_line.js
@@ -4,15 +4,20 @@
     init:function(){
       return $('#four_line .balls').click(function(){
         if(Game.running){
-          var cellToAdd, colNum, currentColor,rowNum, hor_cellToCheck, ver_cellToCheck;
+          var cellToAdd, colNum, currentColor,rowNum, hor_cellToCheck, hor_cell_num, ver_cellToCheck;
           colNum = this.dataset.colNum;
+          rowNum = this.dataset.rowNum;
           currentColor = Game.colors[Game.currentPlayer];
           ver_cellToCheck=$('.balls[data-col-num="' + colNum + '"]');
           cellToAdd=$('.balls[data-col-num="' + colNum + '"][data-cell-type="none"]').last();
-          hor_cellToCheck=cellToAdd[0].dataset.rowNum;
+          hor_cell_num=cellToAdd[0].dataset.rowNum;
           cellToAdd[0].dataset.cellType = currentColor;
           cellToAdd.find('.slot').addClass(currentColor).addClass('bounceInDown');
+          hor_cellToCheck=$('.balls[data-row-num="' + rowNum + '"]');
           Game.check(hor_cellToCheck, ver_cellToCheck);
+          console.log(hor_cell_num);
+          
+          console.log(hor_cellToCheck[0].dataset.cellType);
           return Game.currentPlayer = (Game.currentPlayer + 1 ) % 2;
         }  
       });
@@ -22,17 +27,37 @@
       var i=0;
       var targetCellType = Game.colors[Game.currentPlayer];
       while (i<=3){
-        if ((ver_cellToCheck[i].dataset.cellType == targetCellType) && (ver_cellToCheck[i+1].dataset.cellType == targetCellType) && (ver_cellToCheck[i+2].dataset.cellType == targetCellType) && (ver_cellToCheck[i+3].dataset.cellType == targetCellType)){
+        if ((ver_cellToCheck[i].dataset.cellType == targetCellType)
+          && (ver_cellToCheck[i+1].dataset.cellType == targetCellType) 
+          && (ver_cellToCheck[i+2].dataset.cellType == targetCellType) 
+          && (ver_cellToCheck[i+3].dataset.cellType == targetCellType)){
 
           Game.running=false;
-          if(Game.currentPlayer==0)
-                $('#four_line').prepend('<p>Player 1 wins</p>');
-              else
-                $('#four_line').prepend('<p>Player 2 wins</p>');
-
-          console.log (Game.currentPlayer + "wins")
+          if(Game.currentPlayer==0){
+            $('#four_line').prepend('<p>Player 1 wins</p>');
+          }
+          else{
+            $('#four_line').prepend('<p>Player 2 wins</p>');
+          }
         }
         i+=1;
+      }
+      var j=0;
+      while(j<=4){
+        if ((hor_cellToCheck[j].dataset.cellType == targetCellType) 
+          && (hor_cellToCheck[j+1].dataset.cellType == targetCellType) 
+          && (hor_cellToCheck[j+2].dataset.cellType == targetCellType) 
+          && (hor_cellToCheck[j+3].dataset.cellType == targetCellType)){
+          
+          Game.running=false;
+          if(Game.currentPlayer==0){
+            $('#four_line').prepend('<p>Player 1 wins</p>');
+          }
+          else{
+            $('#four_line').prepend('<p>Player 2 wins</p>');
+          }
+        }
+        j+=1;
       }
     },
     currentPlayer:0,
